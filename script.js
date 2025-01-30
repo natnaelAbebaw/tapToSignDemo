4675d9468c99abe85c351626d6c1cdc1e99d095e
natnaelAbebaw <natnaell70a@gmail.com>
2025-01-30 19:31:13 +0300

    add pages

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-import AirDatepicker from "air-datepicker";
-import "air-datepicker/air-datepicker.css";
-
 async (e) => {
   e.preventDefault();
   e.stopPropagation();
@@ -26,3 +23,42 @@ async (e) => {
 
   document.addEventListener("click", handleOutsideClick);
 };
+
+const slider = document.querySelector(".slides");
+
+const btnSlider1 = document.querySelector(".btn-slider-1");
+const btnSlider2 = document.querySelector(".btn-slider-2");
+const btnSlider3 = document.querySelector(".btn-slider-3");
+
+const slides = document.querySelectorAll(".slide");
+
+let currentIndex = 1;
+const maxIndex = slides.length - 1;
+
+btnSlider1.addEventListener("click", () => {
+  console.log("click");
+  slider.style.transform = `translateX(-${0 * 100}%)`;
+  btnSlider1.classList.add("active");
+  btnSlider2.classList.remove("active");
+  btnSlider3.classList.remove("active");
+  currentIndex++;
+});
+
+btnSlider2.addEventListener("click", () => {
+  console.log("click");
+  slider.style.transform = `translateX(-${1 * 100}%)`;
+
+  btnSlider1.classList.remove("active");
+  btnSlider2.classList.add("active");
+  btnSlider3.classList.remove("active");
+  currentIndex++;
+});
+
+btnSlider3.addEventListener("click", () => {
+  console.log("click");
+  slider.style.transform = `translateX(-${2 * 100}%)`;
+  btnSlider1.classList.remove("active");
+  btnSlider2.classList.remove("active");
+  btnSlider3.classList.add("active");
+  currentIndex++;
+});
